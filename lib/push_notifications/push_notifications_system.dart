b743e17ea805dbdba64848b1744007439d7a944a
Miwa138 <tir.mm1988@gmail.com>
2023-06-21 18:48:36 +0800

    create new project

---
+++ lib/push_notifications/push_notifications_system.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+import '../global/global.dart';
+
+
+class PushNotificationsSystem
+{
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  //notifications arrived/received
+  Future whenNotificationReceived() async
+  {
+    //1. Terminated
+    //When the app is completely closed and opened directly from the push notification
+    FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage? remoteMessage)
+    {
+      if(remoteMessage != null)
+      {
+        //open app and show notification data
+      }
+    });
+
+    //2. Foreground
+    //When the app is open and it receives a push notification
+    FirebaseMessaging.onMessage.listen((RemoteMessage? remoteMessage)
+    {
+      if(remoteMessage != null)
+      {
+        //directly show notification data
+      }
+    });
+
+    //3. Background
+    //When the app is in the background and opened directly from the push notification.
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? remoteMessage)
+    {
+      if(remoteMessage != null)
+      {
+        //open the app - show notification data
+      }
+    });
+  }
+
+  //device recognition token
+  Future generateDeviceRecognitionToken() async
+  {
+    String? registrationDeviceToken = await messaging.getToken();
+
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(sharedPreferences!.getString("uid"))
+        .update(
+        {
+          "userDeviceToken": registrationDeviceToken,
+        });
+
+    messaging.subscribeToTopic("allSellers");
+    messaging.subscribeToTopic("allUsers");
+  }
+}
